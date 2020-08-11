1778d12279c36fffc725febb20ba33548c24dcde
Krzysztof Chruscinski <krzysztof.chruscinski@nordicsemi.no>
2020-08-11 12:03:35 +0200

    samples: peripheral: lpuart: Add support for interrupt driven API
    Added optional support for controlling low power UART using interrupt
    driven API.
    
    Signed-off-by: Krzysztof Chruscinski <krzysztof.chruscinski@nordicsemi.no>

--- samples/peripheral/lpuart/src/main.c
+++ samples/peripheral/lpuart/src/main.c
@@ -13,8 +13,43 @@ LOG_MODULE_REGISTER(app);
 #define BUF_SIZE 64
 static K_MEM_SLAB_DEFINE(uart_slab, BUF_SIZE, 3, 4);
 
+#ifdef CONFIG_UART_INTERRUPT_DRIVEN
+
+static void uart_irq_handler(struct device *dev, void *context)
+{
+	static int repeat = 3;
+	uint8_t buf[] = {1, 2, 3};
+
+	if (uart_irq_tx_ready(dev)) {
+		if (repeat > 0) {
+			uart_fifo_fill(dev, buf, sizeof(buf));
+			repeat --;
+		} else {
+			uart_irq_tx_disable(dev);
+		}
+	}
+
+	if (uart_irq_rx_ready(dev)) {
+		uint8_t buf[10];
+		int len = uart_fifo_read(dev, buf, sizeof(buf));
+		if (len) {
+			printk("read %d bytes\n", len);
+		}
+	}
+}
+
+void interrupt_driven(struct device *dev)
+{
+	uart_irq_callback_set(dev, uart_irq_handler);
+	uart_irq_tx_enable(dev);
+	k_sleep(K_FOREVER);
+}
+
+#else
+
 static void uart_callback(struct device *dev,
-			  struct uart_event *evt, void *user_data)
+			  struct uart_event *evt,
+			  void *user_data)
 {
 	struct device *uart = user_data;
 	int err;
@@ -57,17 +92,11 @@ static void uart_callback(struct device *dev,
 	}
 }
 
-void main(void)
+void async(struct device *lpuart)
 {
-	struct device *lpuart;
 	uint8_t txbuf[5] = {1, 2, 3, 4, 5};
 	int err;
 	uint8_t *buf;
-
-	k_msleep(1000);
-
-	lpuart = device_get_binding("LPUART");
-	__ASSERT(lpuart, "Failed to get the device");
 
 	err = k_mem_slab_alloc(&uart_slab, (void **)&buf, K_NO_WAIT);
 	__ASSERT(err == 0, "Failed to alloc slab");
@@ -84,4 +113,23 @@ void main(void)
 
 		k_sleep(K_MSEC(500));
 	}
+}
+
+#endif
+
+void main(void)
+{
+	struct device *lpuart;
+
+	k_msleep(1000);
+
+	lpuart = device_get_binding("LPUART");
+	__ASSERT(lpuart, "Failed to get the device");
+
+#ifdef CONFIG_NRF_SW_LPUART_INT_DRIVEN
+	interrupt_driven(lpuart);
+#else
+	async(lpuart);
+#endif
+
 }
